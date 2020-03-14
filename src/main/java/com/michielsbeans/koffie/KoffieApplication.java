542f403973f6034faceb984cb17b044333f6395a
Michiel <j.michiel.janssens@gmail.com>
2020-03-14 16:56:46 +0100

    #1

---
+++ src/main/java/com/michielsbeans/koffie/KoffieApplication.java
@@ -0,0 +1,13 @@
+package com.michielsbeans.koffie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoffieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoffieApplication.class, args);
+	}
+
+}
